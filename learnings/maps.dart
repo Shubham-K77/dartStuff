e43d7d3188c1b32366d41ebc85036923fca46b48
Shubham Kadariya <shubham.kadariya021@apexcollege.edu.np>
2025-04-20 19:06:22 +0545

    Learning Maps!

--- learnings/maps.dart
+++ learnings/maps.dart
@@ -1,4 +1,23 @@
 void main() {
-  //Introducing Maps:
-  
+  //Introducing Maps! Key: Value =>
+  var pizzaToppings = {"John": "Pepperoni", "Mary": "Cheese"};
+  print(pizzaToppings);
+  //Show Keys:
+  print(pizzaToppings.keys);
+  //Show Values:
+  print(pizzaToppings.values);
+  //Add A New Pair:
+  pizzaToppings["Tim"] = "Sasuage";
+  print(pizzaToppings);
+  //Add Multiple Values:
+  pizzaToppings.addAll({"Tina": "Bacon", "Steve": "Pineapple"});
+  print(pizzaToppings);
+  //Remove A Key-Value Pair:
+  pizzaToppings.remove("Steve");
+  print(pizzaToppings);
+  //Display The Length
+  print(pizzaToppings.length);
+  //Remove Everything
+  pizzaToppings.clear();
+  print(pizzaToppings);
 }
